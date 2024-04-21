35053a397064143b7ce803f25af5913c73574c54
Nigel Loppie <54985192+nigelloppie@users.noreply.github.com>
2024-04-21 18:23:30 -0300

    Delete day_1/target directory
    removing target directory

--- day_1/target/debug/deps/day_1-ef7c83c9f6ba422e.d
+++
@@ -1,5 +0,0 @@
-D:\coding\rust\aoc\day_1\target\debug\deps\libday_1-ef7c83c9f6ba422e.rmeta: src\main.rs
-
-D:\coding\rust\aoc\day_1\target\debug\deps\day_1-ef7c83c9f6ba422e.d: src\main.rs
-
-src\main.rs:
